34c735affadf69898c790962b0f6b436abdf87d2
Robert <95589243+snoupiii@users.noreply.github.com>
2022-12-08 23:36:21 +0300

    Add files via upload

---
+++ Calculator/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+private slots:
+    void digits_numbers();
+
+
+    void on_pushButton_16_clicked();
+    void operations();
+    void on_pushButton_clear_clicked();
+    void on_pushButton_14_clicked();
+    void math_operations();
+    void on_pushButton_equals_clicked();
+};
+
+
+#endif // MAINWINDOW_H
